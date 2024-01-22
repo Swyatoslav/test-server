8c430dc2ccb6e2a7c85caf67f320bd398c643ea1
sviatoslav <s.iashchenko@exness.com>
2024-01-22 20:19:52 +0200

    Fix main errors

--- main.py
+++ main.py
@@ -20,29 +20,29 @@ async def ping():
 async def get_info(data=Body()):
     if not data:
         raise HTTPException(status_code=400, detail="Empty request body")
-    if not data.get('card_number'):
-        raise HTTPException(status_code=400, detail="No 'card_number' field in request")
-    raw_card_number = data["card_number"]
-    if not raw_card_number:
-        raise HTTPException(status_code=400, detail="Card number can't be empty")
-    if not isinstance(raw_card_number, str):
-        raise HTTPException(status_code=400, detail="Card number should be string")
-    card_number = raw_card_number.strip()
+    if not data.get('bin_number'):
+        raise HTTPException(status_code=400, detail="No 'bin_number' field in request")
+    raw_bin_number = data["bin_number"]
+    if not raw_bin_number:
+        raise HTTPException(status_code=400, detail="BIN number can't be empty")
+    if not isinstance(raw_bin_number, str):
+        raise HTTPException(status_code=400, detail="BIN number should be string")
+    bin_number = raw_bin_number.strip()
     try:
-        float(card_number)
+        float(bin_number)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Card number should contain only digits")
+        raise HTTPException(status_code=400, detail="BIN number should contain only digits")
 
-    if len(card_number) < 5:
-        raise HTTPException(status_code=400, detail="Card number's length can't be less than 5")
+    if len(bin_number) < 5:
+        raise HTTPException(status_code=400, detail="BIN number's length can't be less than 5")
 
-    if len(card_number) > 16:
-        raise HTTPException(status_code=400, detail="Card number's length can't be greater than 16")
+    if len(bin_number) > 16:
+        raise HTTPException(status_code=400, detail="BIN number's length can't be greater than 16")
 
     sleep(3)
-    if card_number not in bin_db.keys():
-        raise HTTPException(status_code=404, detail="Unknown card")
-    return {"data": bin_db[card_number]}
+    if bin_number not in bin_db.keys():
+        raise HTTPException(status_code=404, detail="Unknown BIN")
+    return {"data": bin_db[bin_number]}
 
 
 app.mount("/", StaticFiles(directory="html", html=True))
