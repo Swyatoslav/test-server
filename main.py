96b137c304adebd4c834bcabd7bd643af6402856
sviatoslav <s.iashchenko@exness.com>
2024-01-22 20:38:31 +0200

    Fix main errors

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-import json
 from time import sleep
 
 from fastapi import FastAPI, Body, HTTPException
@@ -20,14 +19,17 @@ async def ping():
 async def get_info(data=Body()):
     if not data:
         raise HTTPException(status_code=400, detail="Empty request body")
-    if not data.get('bin_number'):
+
+    if data.get('bin_number') is None:
         raise HTTPException(status_code=400, detail="No 'bin_number' field in request")
     raw_bin_number = data["bin_number"]
-    if not raw_bin_number:
-        raise HTTPException(status_code=400, detail="BIN number can't be empty")
+
     if not isinstance(raw_bin_number, str):
         raise HTTPException(status_code=400, detail="BIN number should be string")
     bin_number = raw_bin_number.strip()
+
+    if not raw_bin_number:
+        raise HTTPException(status_code=400, detail="BIN number can't be empty")
     try:
         float(bin_number)
     except ValueError:
